65f41170b2d7bf8360c0a1b584bf2e85d92bebe2
Thinker <polarao69@gmail.com>
2020-05-19 10:47:23 +0530

    Update Remove-temp-files.bat

--- Remove-temp-files.bat
+++ Remove-temp-files.bat
@@ -2,7 +2,7 @@
 color 17
 del "%tmp%\*.*" /s /q /f
 FOR /d %%p IN ("%tmp%\*.*") DO rmdir "%%p" /s /q
-start C:\Windows\Prefetch\*"
-start C:\Windows\Temp\*"
-start C:\Windows\system32\tree.com
+del C:\Windows\Prefetch\*"
+del C:\Windows\Temp\*"
+del C:\Windows\system32\tree.com
 pause>nul
